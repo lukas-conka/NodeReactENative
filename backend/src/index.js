66518293b4cfe7e2cb1f60d4efe4a888c5687061
Lucas Santos <lucas.santos@pcservice.com.br>
2020-01-15 18:30:16 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://lukasconka:5Csl3dVcgnatKcqY@cluster0-svoda.mongodb.net/week10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true })
+
+//o arquivo de Routes deve vir depois 
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333)
