55a8e90698c8d71c8d7f5928a0a0477a655f4bf0
Lucas Santos <lucas.santos@pcservice.com.br>
2020-01-15 20:10:22 -0300

    rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
-const { Router } = require('express')
+const {Router} = require('express')
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-    console.log(req.body)
-    return res.json({message: 'Hello Portugal!'})
-});
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
+
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
