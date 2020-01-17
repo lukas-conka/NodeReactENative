bb23890c0190da615a7799de7c0145b576e2575e
Lucas Santos <lucas.santos@pcservice.com.br>
2020-01-16 21:17:40 -0300

    aula4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express();
 
 mongoose.connect('mongodb+srv://lukasconka:5Csl3dVcgnatKcqY@cluster0-svoda.mongodb.net/week10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors())
 //o arquivo de Routes deve vir depois 
 app.use(express.json());
 app.use(routes);
