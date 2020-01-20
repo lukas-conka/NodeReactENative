06d6fef55054b684263adc46e945f14f12f81591
Lucas Amaral <lukas@hotmail.co.jp>
2020-01-19 23:14:40 -0300

    start mobile

--- web/src/App.js
+++ web/src/App.js
@@ -17,9 +17,7 @@ import DevForm from './components/DevForm';
 function App() {
 
   const [devs, setDevs] = useState([]);
-
-   
-
+  
     useEffect(() => {
         async function loadDevs() {
             const response = await api.get('/devs');
